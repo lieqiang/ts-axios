34137d0583969d903997cf81a973206ed10ccdde
霸财-邹烈强 <bc_zoulq@bc.com>
2019-10-22 17:41:30 +0800

    [build] 0.0.4

--- release.sh
+++ release.sh
@@ -7,14 +7,14 @@ echo "Enter release version:"
 echo # (optional) move to a new line
 #if [[ $REPLAY =~ ^[Yy]$ ]]
 #then
-echo "release 0.0.3 ..."
+echo "release 0.0.4 ..."
 
 # 提交变化文件到暂存区
 git add -A
 # 提交代码 注释字段为："[build] $VERSION"
-git commit -m "[build] 0.0.3"
+git commit -m "[build] 0.0.4"
 # 提交一条修改记录 注释："[release] $VERSION"
-npm version "0.0.3" --message "[release] 0.0.3"
+npm version "0.0.4" --message "[release] 0.0.4"
 git push origin master
 
 # pulish 代码发布到npm
